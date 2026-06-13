e3c2938e22155474d6d34991bf1772db0430367e
Bobby <bobby2009.liu@gmail.com>
2026-06-13 11:27:40 -0400

    Motor Control

--- src/motor_control/motorTest.py
+++ src/motor_control/motorTest.py
@@ -3,17 +3,13 @@ import time
 import sys
 
 STEP_PIN = 12  # Connects to STEP on TMC2208
-DIR_PIN = 16  # Connects to DIR on TMC2208
-
+DIR_PIN = 16   # Connects to DIR on TMC2208
 
 def setup_gpio():
 
     GPIO.setmode(GPIO.BOARD)
-
-    # Configure pins as outputs and set them to LOW initially
     GPIO.setup(STEP_PIN, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(DIR_PIN, GPIO.OUT, initial=GPIO.LOW)
-
 
 def move_motor(steps, direction, speed_delay=0.001):
 
@@ -27,21 +23,23 @@ def move_motor(steps, direction, speed_delay=0.001):
         GPIO.output(STEP_PIN, GPIO.LOW)
         time.sleep(speed_delay)
 
+def move_right(steps, speed_delay=0.001):
 
+    move_motor(steps, direction=GPIO.HIGH, speed_delay=speed_delay)
+
+def move_left(steps, speed_delay=0.001):
+
+    move_motor(steps, direction=GPIO.LOW, speed_delay=speed_delay)
+
+def charge_launch():
+    move_motor(149.94, direction=GPIO.LOW, speed_delay=0.001)
 if __name__ == "__main__":
     try:
         setup_gpio()
 
-
-        move_motor(steps=200, direction=GPIO.HIGH, speed_delay=0.002)
-
-        # Pause for 1 second
+        move_right(steps=200, speed_delay=0.002)
         time.sleep(1)
-
-        move_motor(steps=200, direction=GPIO.LOW, speed_delay=0.002)
-
-
+        move_left(steps=200, speed_delay=0.002)
 
     finally:
-
         GPIO.cleanup()
